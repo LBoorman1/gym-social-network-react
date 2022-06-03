b085b6439d3ea55173b39ff8e72728425fd9a117
LukeBWood <boormanluke76@gmail.com>
2022-06-03 19:08:29 +0100

    sizing is still screwed but moving works

---
+++ src/pages/GymMap.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Map from '../components/Map'
+
+function GymMap() {
+  return (
+    <div className='relative'>
+        <Map />
+    </div>
+    
+  )
+}
+
+export default GymMap
