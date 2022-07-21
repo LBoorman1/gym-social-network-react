1481bea3a4674024f8950027190436651e88ba60
LukeBWood <boormanluke76@gmail.com>
2022-07-21 23:37:31 +0100

    Adding a post works when logged in and the creator of the post has the id of the correct user.

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,12 +1,13 @@
 import PostMessage from "../models/postMessage.js";
 
 export const createPost = async (req, res) => {
-  const post = req.body;
-  const newPost = new PostMessage(post);
+  const post = req.body.postMessage;
+  const id = req.id;
+  const newPost = new PostMessage({ postMessage: post, creator: id });
 
   try {
     await newPost.save();
-    res.status(201).json(newPost);
+    res.status(201);
   } catch (error) {
     res.status(409).json({ messsage: error.message });
   }
--- backend/models/postMessage.js
+++ backend/models/postMessage.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    message: String,
-    likeCount: {
-        type: Number,
-        default: 0
-    },
-    createdAt: {
-        type: Date,
-        default: new Date()
-    },
-    creator: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: 'User',
-    }
+  postMessage: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  creator: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
 });
 
-const PostMessage = mongoose.model('PostMessage', postSchema);
+const PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
