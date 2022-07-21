0f92051a0c3bec8186a38e029ada545126cdb0df
LukeBWood <boormanluke76@gmail.com>
2022-07-21 22:57:31 +0100

    middleware function seems to work correctly, think id is not being grabbed correctly as it is becoming undefined in the web console.

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,34 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import userRoutes from './routes/users.js';
-import authRoutes from './routes/auth.js';
-import dotenv from 'dotenv';
-import { User } from './models/user.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+import { User } from "./models/user.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://lukebwood:lukebwood123@cluster0.8ukh4lu.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "REDACTED";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, ()=> console.log("Server running on :" + PORT)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log("Server running on :" + PORT)))
+  .catch((error) => console.log(error.message));
 
 const findUsers = async () => {
-    const users = await User.find({});
-    console.log(users);
-}
+  const users = await User.find({});
+  console.log(users);
+};
+
+findUsers();
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { getPosts, createPost } from '../controllers/posts.js';
+import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
-router.get('/', getPosts); //http://localhost:5000/posts
-router.post('/', createPost);
+router.get("/", getPosts); //http://localhost:5000/posts
+router.post("/", authenticateToken, createPost);
 
 export default router;
