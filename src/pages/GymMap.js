84487f601edb7d212829fceacac955d8c4b2f2f5
LukeBWood <boormanluke76@gmail.com>
2022-10-25 17:06:06 +0100

    Moving files around and sorting imports

--- src/pages/GymMap.js
+++ src/pages/GymMap.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Map from '../components/Map'
-import NavBar from '../components/NavBar'
+import React from "react";
+import Map from "../components/Map";
+import NavBar from "../components/navbarComponents/NavBar";
 
 function GymMap() {
   return (
-    <body id='gymmap'> {/* This is used to prevent window scrolling on the gym map page */}
-    <div className=''>
-      <NavBar />
-      <Map />
-    </div>
+    <body id="gymmap">
+      {" "}
+      {/* This is used to prevent window scrolling on the gym map page */}
+      <div className="">
+        <NavBar />
+        <Map />
+      </div>
     </body>
-  )
+  );
 }
 
-export default GymMap
+export default GymMap;
