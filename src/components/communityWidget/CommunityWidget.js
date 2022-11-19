42eba9b746c6939e6294a4d9795ba3add33b70ea
LukeBWood <boormanluke76@gmail.com>
2022-11-19 17:47:29 +0000

    completed more frontend work

--- src/components/communityWidget/CommunityWidget.js
+++ src/components/communityWidget/CommunityWidget.js
@@ -4,6 +4,9 @@ import Community from "./Community";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setCommunities } from "../../redux/reducers/CommunitiesSlice";
+import { PlusIcon, SearchIcon, UserGroupIcon } from "@heroicons/react/solid";
+import { setSearchOpen } from "../../redux/reducers/SearchModalSlice";
+import { setCommunityOpen } from "../../redux/reducers/CommunityModalSlice";
 
 function CommunityWidget() {
   const token = localStorage.getItem("token");
@@ -35,15 +38,34 @@ function CommunityWidget() {
   }, []);
 
   return (
-    <div className="hidden md:block sticky top-[112px] bg-[#D9D9D9] mt-2 mx-2 p-2 h-[50vh] rounded-md z-0 basis-1/4">
-      {communities.map((community) => (
-        <Community
-          key={community.community._id}
-          _id={community.community._id}
-          name={community.community.communityName}
-          active={activeCommunity === community.community._id ? true : false}
-        />
-      ))}
+    <div className="hidden md:flex flex-col items-center sticky top-[112px] bg-[#D9D9D9] mt-2 mx-2 p-2 h-fit rounded-md z-0 basis-1/4">
+      <div className="w-full">
+        {communities.map((community) => (
+          <Community
+            key={community.community._id}
+            _id={community.community._id}
+            name={community.community.communityName}
+            active={activeCommunity === community.community._id ? true : false}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row justify-between w-3/4 mb-2 ">
+        <button
+          className="hover:bg-[#A6A6A6] p-2 rounded-md"
+          onClick={() => dispatch(setSearchOpen())}
+        >
+          <SearchIcon className="h-5 w-5" />
+        </button>
+        <button
+          className="hover:bg-[#A6A6A6] p-2 rounded-md"
+          onClick={() => dispatch(setCommunityOpen())}
+        >
+          <PlusIcon className="h-5 w-5" />
+        </button>
+        <button className="hover:bg-[#A6A6A6] p-2 rounded-md">
+          <UserGroupIcon className="h-5 w-5" />
+        </button>
+      </div>
     </div>
   );
 }
