a0d140a747031774a88e5fbf6f8869de6c2404fc
LukeBWood <boormanluke76@gmail.com>
2023-01-03 19:58:50 +0000

    Created the routes and the controller for leaderBoards, create leader Boards has been programmed but needs to be tested.

---
+++ backend/controllers/leaderBoards.js
@@ -0,0 +1,33 @@
+import leaderBoard from "../models/leaderBoard";
+
+export const createLeaderBoard = async (req, res) => {
+  const communityId = req.body.communityId;
+  const creatorId = req.id;
+  const title = req.body.title;
+
+  const newLeaderBoard = new leaderBoard({
+    community: communityId,
+    owner: creatorId,
+    title: title,
+  });
+
+  try {
+    const invalidLeaderBoard = await leaderBoard.findOne({
+      community: communityId,
+      title: title,
+    });
+
+    if (invalidLeaderBoard) {
+      return res
+        .status(409)
+        .send({
+          message:
+            "Leader Board already exists in this community - Try a different name",
+        });
+    }
+    await newLeaderBoard.save();
+    res.status(201);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
