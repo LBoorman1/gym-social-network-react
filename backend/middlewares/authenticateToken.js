a1db8a693cf3b1443866859c74090e02830edaa9
LukeBWood <boormanluke76@gmail.com>
2023-01-03 19:33:46 +0000

    wrapping up slightly

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -1,19 +1,25 @@
 import jwt from "jsonwebtoken";
 
-//middleware function to dtermine user from passed token
 export const authenticateToken = (req, res, next) => {
+  //Takes the encrypted token from the request headers.
   const authHeader = req.headers["authorisation"];
   const token = authHeader && authHeader.split(" ")[1];
 
+  //Simple check to see if the token exists
   if (token == null) {
     return res.status(403).send({ message: "token not found" });
   }
 
+  //Uses the JWTPRIVATEKEY to verify the token is related to a user
   jwt.verify(token, process.env.JWTPRIVATEKEY, (err, id) => {
     if (err) {
       return res.status(403).send({ message: err.message });
     }
+
+    //Adds the verified userId into the request body
     req.id = id;
+
+    //Calls next to move into the requested end point
     next();
   });
 };
