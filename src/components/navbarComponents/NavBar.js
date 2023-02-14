9861aa9eb61364d5c81f24d11d32c78e662efe32
LukeBWood <boormanluke76@gmail.com>
2023-02-14 14:21:09 +0000

    Added the logo to the navbar component

--- src/components/navbarComponents/NavBar.js
+++ src/components/navbarComponents/NavBar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import HamburgerButton from "./HamburgerButton";
+import mainLogo from "../../FitFeedLogoNew.png";
 
 function NavBar() {
   return (
     <div className="sticky top-0 flex w-100 h-[100px] px-5 py-5 bg-[#79ADDC] justify-between items-center z-10 drop-shadow-xl border-b-[1px] border-neutral-600 border-solid">
       {/* Logo Section */}
-      <div>
-        <span className="rounded-full bg-white h-[40px] w-[40px] inline-block mr-2">
-          logo
-        </span>
+      <div className="flex justify-start w-full">
+        <img src={mainLogo} alt="fitfeedlogo" className="h-[80px]" />
       </div>
 
       <div className="flex gap-1">
