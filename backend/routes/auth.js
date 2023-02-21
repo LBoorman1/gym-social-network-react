65c448a18409fa1c63bf8b291bd231439fc28693
LukeBWood <boormanluke76@gmail.com>
2023-02-21 17:47:37 +0000

    made the frontned look better

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,45 +1,43 @@
-import express from 'express';
-import { User } from '../models/user.js';
+import express from "express";
+import { User } from "../models/user.js";
 import joi from "joi";
 import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-router.post("/", async(req, res) => {
-    try {
-        const users = await User.find({});
-        console.log(users);
-        const {error} = validate(req.body);
-        if(error) {
-            return res.status(400).send({ message: error.details[0].message});
-        }
-        const user = await User.findOne({emailAddress: req.body.emailAddress });
-        if(!user) {
-            return res.status(401).send({ message: "Invalid email or password"});
-        }
-        const validPassword = await bcrypt.compare(
-            req.body.password, user.password
-        );
-        if(!validPassword) {
-            return res.status(401).send({ message: "Invalid email or password"});
-        }
-
-        const token = user.generateAuthToken();
-        
-        res.status(200).send({data: token, message: "Logged in Successfully"});
-
-    } catch (error) {
-        res.status(500).send({message: "Internal Server Error"});
-        console.log(error);
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error) {
+      return res.status(400).send({ message: error.details[0].message });
     }
+    const user = await User.findOne({ emailAddress: req.body.emailAddress });
+    if (!user) {
+      return res.status(401).send({ message: "Invalid email or password" });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).send({ message: "Invalid email or password" });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(200).send({ data: token, message: "Logged in Successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+    console.log(error);
+  }
 });
 
 const validate = (data) => {
-    const schema = joi.object({
-        emailAddress: joi.string().email().required().label("emailAddress"),
-        password: joi.string().required().label("password")
-    })
-    return schema.validate(data)
-}
+  const schema = joi.object({
+    emailAddress: joi.string().email().required().label("emailAddress"),
+    password: joi.string().required().label("password"),
+  });
+  return schema.validate(data);
+};
 
 export default router;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,90 +1,120 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 function Register() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    emailAddress: "",
+  });
 
-    const [data, setData] = useState({
-        username: "",
-        password: "",
-        emailAddress: ""
-    });
+  const navigate = useNavigate();
 
-    const handleChange = ({currentTarget: input}) => {
-        setData({...data, [input.name]: input.value});
-    };
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const url = "http://localhost:5000/api/users";
-            const {data:res} = await axios.post(url, data);
-            console.log(res.message);
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const url = "http://localhost:5000/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/Login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
-    <div className='flex items-center justify-center p-10 h-full' style={{ 
-        backgroundImage: `url("https://media.istockphoto.com/photos/dumbbells-and-kettlebells-on-a-floor-bodybuilding-equipment-fitness-picture-id1307360297?k=20&m=1307360297&s=170667a&w=0&h=aRkT_smYFrXxjAK8ahmXCyVD_vESyYltkipD9qjjEw8=")`,
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize:'cover',
-        width:'100vw',
-        height:'100vh'
-    }}> {/* The background */}
-        <form className='flex flex-col items-center justify-center bg-white rounded' onSubmit={handleSubmit}> {/*box around the form */}
-            <div className='pt-5'> {/*  The Register title */}
-                <h1 className='text-3xl'>REGISTER</h1>
-            </div> 
-            <div className='flex flex-col md:max-w-3/4 text-xl p-10'> {/* Box for the elements of the form */} 
-                           
-                <div className='flex flex-col md:flex-row md:justify-between p-2 md:w-2/4'> {/* This will be username and password */}
-                    <div className='flex flex-col pr-2'> {/* One box for username */}
-                        <label className='text-left'>Username</label>    
-                        <input 
-                            className='bg-gray-400 rounded placeholder-gray-300' 
-                            type="text" 
-                            placeholder='Username123'
-                            name='username' 
-                            onChange={handleChange}
-                            value={data.username}
-                            required
-                        />
-                    </div>
-                    <div className='flex flex-col'> {/* One box for password */}
-                        <label className='text-left'>Password</label>    
-                        <input 
-                            className='bg-gray-400 rounded shadow-lg placeholder-gray-300 no-underline' 
-                            type="text"    
-                            placeholder='************' 
-                            name='password'
-                            onChange={handleChange}
-                            value={data.password}
-                            required
-                        />
-                    </div>
-                </div>
-                <div className='w-full' > {/* This is for email address */}
-                    <div className='flex flex-col p-2 '> {/* One box for username */}
-                        <label className='text-left'>Email Address</label>    
-                        <input 
-                            className='bg-gray-400 rounded placeholder-gray-300 no-underline' 
-                            type="text" 
-                            placeholder='Email Address'
-                            name='emailAddress'
-                            onChange={handleChange}
-                            value={data.emailAddress}
-                            required
-                        />
-                    </div>
-                </div>
+    <div
+      className="flex items-center justify-center h-full"
+      //   style={{
+      //     backgroundImage: `url("https://media.istockphoto.com/photos/dumbbells-and-kettlebells-on-a-floor-bodybuilding-equipment-fitness-picture-id1307360297?k=20&m=1307360297&s=170667a&w=0&h=aRkT_smYFrXxjAK8ahmXCyVD_vESyYltkipD9qjjEw8=")`,
+      //     backgroundPosition: "center",
+      //     backgroundRepeat: "no-repeat",
+      //     backgroundSize: "cover",
+      //     width: "100vw",
+      //     height: "100vh",
+      //   }}
+    >
+      <div className="flex w-1/2 h-full bg-sky-100"></div>
+
+      <form
+        className="flex flex-col items-center justify-center bg-white rounded w-1/2 h-full"
+        onSubmit={handleSubmit}
+      >
+        {" "}
+        {/*box around the form */}
+        <div className="pt-5">
+          {" "}
+          {/*  The Register title */}
+          <h1 className="text-3xl font-poppins font-bold text-sky-700">
+            REGISTER
+          </h1>
+        </div>
+        <div className="flex flex-col md:max-w-3/4 text-xl p-10">
+          {" "}
+          {/* Box for the elements of the form */}
+          <div className="flex flex-col md:flex-row md:justify-between p-2 md:w-2/4">
+            {" "}
+            {/* This will be username and password */}
+            <div className="flex flex-col pr-2">
+              {" "}
+              {/* One box for username */}
+              <label className="text-left font-poppins">Username</label>
+              <input
+                className="bg-sky-100 p-5 rounded placeholder-sky-700 font-poppins placeholder:opacity-30 text-sky-700"
+                type="text"
+                placeholder="Username123"
+                name="username"
+                onChange={handleChange}
+                value={data.username}
+                required
+              />
             </div>
-            <button type='submit' className='bg-gray-400 rounded m-5 p-2'>
-                Submit
-            </button>
-        </form>        
+            <div className="flex flex-col">
+              {" "}
+              {/* One box for password */}
+              <label className="text-left font-poppins">Password</label>
+              <input
+                className="bg-sky-100 p-5 rounded placeholder-sky-700 font-poppins placeholder:opacity-30 text-sky-700"
+                type="password"
+                placeholder="**********"
+                name="password"
+                onChange={handleChange}
+                value={data.password}
+                required
+              />
+            </div>
+          </div>
+          <div className="w-full">
+            {" "}
+            {/* This is for email address */}
+            <div className="flex flex-col p-2 ">
+              {" "}
+              {/* One box for username */}
+              <label className="text-left font-poppins">Email Address</label>
+              <input
+                className="bg-sky-100 p-5 rounded placeholder-sky-700 font-poppins placeholder:opacity-30 text-sky-700"
+                type="text"
+                placeholder="emailaddress@email.com"
+                name="emailAddress"
+                onChange={handleChange}
+                value={data.emailAddress}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-sky-100 rounded p-5 hover:bg-sky-200 text-sky-700 font-poppins font-bold"
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
