561ed28d867d283a92a00ee25313ca16bcc5f7bd
LukeBWood <boormanluke76@gmail.com>
2023-02-21 18:16:30 +0000

    Users now upload profile photo, need to make them viewable in the front end next

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,36 +1,56 @@
-import express from 'express'
-import { User, validate } from '../models/user.js'
-import bcrypt from "bcrypt"
+import express from "express";
+import { User, validate } from "../models/user.js";
+import bcrypt from "bcrypt";
+import cloudinary from "../utils/cloudinary.js";
 
 const router = express.Router();
 
-router.post("/", async(req, res) => {
-    try {
-        const {error} = validate(req.body);
-        if(error) {
-            return res.status(400).send({ message: error.details[0].message});
-        }
-        
-        const email = await User.findOne({ emailAddress: req.body.emailAddress });
-        if(email) {
-            return res.status(409).send({ message: "User with given email address already exists!"});
-        }
-        const username = await User.findOne({ emailAddress: req.body.emailAddress });
-        if(username) {
-            return res.status(409).send({ message: "User with given username already exists!"});
-        }
-
-        const salt = await bcrypt.genSalt(Number(process.env.SALT));
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-        await new User({...req.body, password:hashPassword}).save();
-        res.status(201).send({ message: "User created successfully"})
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({message: "Internal Server Error"});
+router.post("/", async (req, res) => {
+  const image = req.body.image;
+  try {
+    const { error } = validate(req.body);
+    if (error) {
+      return res.status(400).send({ message: error.details[0].message });
     }
+
+    const email = await User.findOne({ emailAddress: req.body.emailAddress });
+    if (email) {
+      return res
+        .status(409)
+        .send({ message: "User with given email address already exists!" });
+    }
+    const username = await User.findOne({
+      emailAddress: req.body.emailAddress,
+    });
+    if (username) {
+      return res
+        .status(409)
+        .send({ message: "User with given username already exists!" });
+    }
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const result = await cloudinary.uploader.upload(image, {
+      folder: "profilePics",
+    });
+
+    await new User({
+      username: req.body.username,
+      password: hashPassword,
+      emailAddress: req.body.emailAddress,
+      image: {
+        public_id: result.public_id,
+        url: result.secure_url,
+      },
+    }).save();
+
+    // await new User({ ...req.body, password: hashPassword }).save();
+    res.status(201).send({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 export default router;
